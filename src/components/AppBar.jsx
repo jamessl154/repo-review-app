48832ce410ebcfdf82f09a12f76e7b452cfbf91c
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-11-22 15:05:50 +0000

    added horizontal ScrollView for AppBar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import View from './View';
 import AppBarTab from './AppBarTab';
 import Constants from 'expo-constants';
@@ -8,7 +8,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "center",
-    height: 58,
+    height: 68,
     paddingLeft: 5,
     paddingRight: 5,
     paddingTop: Constants.statusBarHeight,
@@ -18,9 +18,20 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container} black>
-      <AppBarTab tabText="Repositories" to="/" />
-      <AppBarTab tabText="Sign In" to="/signin" />
-      <AppBarTab tabText="TODO" to="TODO" />
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <AppBarTab tabText="Repositories" to="/" />
+        <AppBarTab tabText="Sign In" to="/signin" />
+        <AppBarTab tabText="TODO" to="TODO" />
+        <AppBarTab tabText="Complete" to="/" />
+        <AppBarTab tabText="Test" to="/signin" />
+        <AppBarTab tabText="Of the" to="TODO" />
+        <AppBarTab tabText="New" to="/" />
+        <AppBarTab tabText="Horizontal" to="/signin" />
+        <AppBarTab tabText="Scrollbar" to="TODO" />
+        <AppBarTab tabText="Implementation" to="/" />
+        <AppBarTab tabText="Is Now" to="/signin" />
+        <AppBarTab tabText="Finished" to="TODO" />
+      </ScrollView>
     </View>
   );
 };
